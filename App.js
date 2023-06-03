d9b79e44be35d3136a6f2f1d64fd655599b8eb05
108062371 <giny@gapp.nthu.edu.tw>
2023-06-04 01:01:26 +0800

    add: HomeScreen

--- App.js
+++ App.js
@@ -1,26 +1,52 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, KeyboardAvoidingView } from "react-native";
 import { withAuthenticator } from "aws-amplify-react-native";
 import { Amplify } from "aws-amplify";
+import { NavigationContainer } from "@react-navigation/native";
 import awsExports from "./src/aws-exports";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createStackNavigator } from "@react-navigation/stack";
+//components
+import HomeScreen from "./src/screens/HomeScreen";
+
 Amplify.configure(awsExports);
 
+const screens = [{ name: "HomeScreen", component: HomeScreen }];
+
 const App = () => {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+            style={{ flex: 2 }}
+          >
+            <Stack.Navigator initialRouteName="Home">
+              {screens.map((screen) => (
+                <Stack.Screen
+                  key={screen.name}
+                  name={screen.name}
+                  component={screen.component}
+                  options={{ headerShown: false }}
+                />
+              ))}
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </>
   );
 };
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  addButton: {
+    marginRight: 20,
+  },
+  logOutBtn: {
+    marginLeft: 10,
   },
 });
 export default withAuthenticator(App);
